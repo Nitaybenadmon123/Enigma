28d61c7f181beaacef37d8d57daaec3bba573ca5
Idantegave7 <158479599+Idantegave7@users.noreply.github.com>
2025-11-22 13:03:00 +0200

    עדכון שלב 6 סופי

--- Enigma/main.py
+++ Enigma/main.py
@@ -2,6 +2,62 @@ from core.Rotor import Rotor
 from core.Reflector import Reflector
 from core.Plugboard import Plugboard
 from core.Enigma import Enigma
+import cProfile
+import pstats
+def build_enigma():
+    # Rotor I
+    rotor_I = Rotor(
+        permutation="EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        notch="Q",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Rotor II
+    rotor_II = Rotor(
+        permutation="AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        notch="E",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Rotor III
+    rotor_III = Rotor(
+        permutation="BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        notch="V",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Reflector
+    reflector_B = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
+
+    # Empty plugboard (לפי הטבלה)
+    plugboard = Plugboard([])
+
+    # Create machine – using your constructor format
+    return Enigma(
+        left=rotor_I,
+        middle=rotor_II,
+        right=rotor_III,
+        reflector=reflector_B,
+        plugboard=plugboard
+    )
+def benchmark():
+    msg = "HELLOWORLD"
+    for _ in range(2000):   # כמות נכבדת לבדיקה
+        enigma = build_enigma()
+        enigma.encrypt_message(msg)
+def run_profile():
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    benchmark()
+
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats("tottime")
+    stats.print_stats(25)   # מציג את 25 הפונקציות הכי כבדות
+
 
 
 def main():
@@ -32,7 +88,20 @@ def main():
         ring_setting=0,
         offset=0
     )
-
+    # Rotor IIII
+    rotor_IIII = Rotor(
+        permutation="ESOVPZJAYQUIRHXLNFTGKDCMWB",
+        notch="J",
+        ring_setting=0,
+        offset=0
+    )
+    # Rotor IIIII
+    rotor_IIIII = Rotor(
+        permutation="VZBRGITYUPSDNHLXAWMJQOFECK",
+        notch="Z",
+        ring_setting=0,
+        offset=0
+    )
     # ----------------------------
     # 2. רפלקטור B
     # ----------------------------
@@ -71,4 +140,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()            # זה מריץ את התרגיל הרגיל שלך
+    run_profile()        # זה מפעיל את בדיקת הביצועים (Task 6)
+    
