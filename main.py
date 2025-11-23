0088818546a5c58d03a2373584fe896bd03de753
Nitaybenadmon123 <nitaybe@ac.sce.ac.il>
2025-11-23 14:46:13 +0200

    שמירה עד תרגיל 6 כולל

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
 # main.py
-
+# Nitay Ben Admon 207781790
+# Idan Tegave 207329137
 from core.Rotor import Rotor
 from core.Reflector import Reflector
 from core.Plugboard import Plugboard
 from core.Enigma import Enigma
+import cProfile
+import pstats
+def build_enigma():
+    # Rotor I
+    rotor_I = Rotor(
+        permutation="EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        notch="Q",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Rotor II
+    rotor_II = Rotor(
+        permutation="AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        notch="E",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Rotor III
+    rotor_III = Rotor(
+        permutation="BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        notch="V",
+        ring_setting=0,
+        offset=0
+    )
+
+    # Reflector
+    reflector_B = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
+
+    # Empty plugboard (לפי הטבלה)
+    plugboard = Plugboard([])
+
+    # Create machine – using your constructor format
+    return Enigma(
+        left=rotor_I,
+        middle=rotor_II,
+        right=rotor_III,
+        reflector=reflector_B,
+        plugboard=plugboard
+    )
+def benchmark():
+    msg = "HELLOWORLD"
+    for _ in range(2000):   # כמות נכבדת לבדיקה
+        enigma = build_enigma()
+        enigma.encrypt_message(msg)
+
+
+def run_profile():
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    benchmark()
+
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats("tottime")
+    stats.print_stats(25)   # מציג את 25 הפונקציות הכי כבדות
+
 def run_table_test():
     # Rotors I-II-III
     rotor_I = Rotor(
@@ -25,6 +84,21 @@ def run_table_test():
         notch="V",
         ring_setting=0,
         offset=21  # V = 22 → offset=21
+    )
+
+    # Rotor IIII
+    rotor_IIII = Rotor(
+        permutation="ESOVPZJAYQUIRHXLNFTGKDCMWB",
+        notch="J",
+        ring_setting=0,
+        offset=0
+    )
+    # Rotor IIIII
+    rotor_IIIII = Rotor(
+        permutation="VZBRGITYUPSDNHLXAWMJQOFECK",
+        notch="Z",
+        ring_setting=0,
+        offset=0
     )
 
     reflector_B = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
@@ -104,3 +178,4 @@ def main():
 if __name__ == "__main__":
     main()
     run_table_test()
+    run_profile()
